38bec8eac1a3ac8168fe06f552b1d2fcdbbdbbc5
Brage Skjønborg <bskjon@outlook.com>
2026-03-29 15:09:28 +0200

    build

--- ffmpeg-build/libs/build_libaom.py
+++ ffmpeg-build/libs/build_libaom.py
@@ -3,14 +3,28 @@ from utils import run
 
 def main():
     run(["sudo", "apt-get", "update"])
-    run(["sudo", "apt-get", "install", "-y", "git", "cmake", "ninja-build", "build-essential"])
+    run([
+        "sudo", "apt-get", "install", "-y",
+        "git",
+        "cmake",
+        "ninja-build",
+        "build-essential",
+        "nasm",
+        "yasm"
+    ])
 
     run(["git", "clone", "--depth=1", "https://aomedia.googlesource.com/aom"])
-    run(["bash", "-c",
-         "cd aom && mkdir -p build && cd build && "
-         "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release "
-         "-DBUILD_SHARED_LIBS=OFF -DENABLE_TESTS=OFF -DENABLE_EXAMPLES=OFF .. && "
-         "ninja && sudo ninja install"])
+    run([
+        "bash", "-c",
+        "cd aom && mkdir -p build && cd build && "
+        "cmake -G Ninja "
+        "-DCMAKE_BUILD_TYPE=Release "
+        "-DBUILD_SHARED_LIBS=OFF "
+        "-DENABLE_TESTS=OFF "
+        "-DENABLE_EXAMPLES=OFF "
+        ".. && "
+        "ninja && sudo ninja install"
+    ])
     run(["sudo", "ldconfig"])
 
 if __name__ == "__main__":
--- ffmpeg-build/libs/build_svtav1.py
+++ ffmpeg-build/libs/build_svtav1.py
@@ -3,14 +3,31 @@ from utils import run
 
 def main():
     run(["sudo", "apt-get", "update"])
-    run(["sudo", "apt-get", "install", "-y", "git", "cmake", "ninja-build", "build-essential", "pkg-config"])
+    run([
+        "sudo", "apt-get", "install", "-y",
+        "git",
+        "cmake",
+        "ninja-build",
+        "build-essential",
+        "pkg-config",
+        "nasm",
+        "yasm"
+    ])
 
     run(["git", "clone", "--recurse-submodules", "https://gitlab.com/AOMediaCodec/SVT-AV1.git"])
-    run(["bash", "-c",
-         "cd SVT-AV1 && mkdir -p build && cd build && "
-         "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release "
-         "-DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && "
-         "ninja && sudo ninja install"])
+
+    run([
+        "bash", "-c",
+        "cd SVT-AV1 && "
+        "mkdir -p build && cd build && "
+        "cmake -G Ninja "
+        "-DCMAKE_BUILD_TYPE=Release "
+        "-DBUILD_SHARED_LIBS=OFF "
+        "-DCMAKE_POSITION_INDEPENDENT_CODE=ON "
+        ".. && "
+        "ninja && sudo ninja install"
+    ])
+
     run(["sudo", "ldconfig"])
 
 if __name__ == "__main__":
